53aa437beee4fbb7b9dcdd9e5dc69d12f449c1e2
Casey DeLorme <cdelorme@gmail.com>
2015-08-06 21:36:47 -0400

    working torrent list process

--- main.go
+++ main.go
@@ -1,15 +1,47 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/cdelorme/go-config"
 	"github.com/cdelorme/go-log"
 	"github.com/cdelorme/go-maps"
 )
 
-type settings struct{}
-type torrent struct{}
+const (
+	GetSession     = "{\"method\":\"session-get\"}"
+	GetTorrents    = "{\"method\":\"torrent-get\",\"arguments\":{\"fields\":[\"id\",\"isFinished\",\"downloadDir\",\"files\"]}}"
+	RemoveTorrents = ""
+)
+
+type settings struct {
+	Port int    `json:"rpc-port"`
+	Uri  string `json:"rpc-url"`
+}
+
+type file struct {
+	Name string `json:"name"`
+}
+
+type torrent struct {
+	Id       int    `json:"id"`
+	Files    []file `json:""files""`
+	Path     string `json:"downloadDir"`
+	Finished bool   `json:"isFinished"`
+}
+
+type internal struct {
+	Torrents []torrent `json:"torrents"`
+}
+
+type response struct {
+	Args   internal `json:"arguments"`
+	Result string   `json:"result"`
+}
 
 func main() {
 	logger := &log.Logger{}
@@ -28,18 +60,51 @@ func main() {
 	}
 	maps.To(conf, l)
 
-	remove(logger, list(logger, conf), path)
+	url := rpc(conf)
+	token := session(url)
+	finished := done(url, token)
+	remove(url, finished, token, path)
 }
 
-func remove(logger *log.Logger, torrents []torrent, path string) bool {
-	// loop torrents
-	// send remove requests to transmission
-	// if path is not empty attempt to relocate the files related to the now-removed torrent
-	return true
+func remove(route string, torrents *[]torrent, session string, path string) {
+
+	// create removal list
+
+	// remove torrents
+
+	// if not successful, exit before causing damage
+
+	if path != "" {
+		// move files to path
+	}
 }
 
-func list(logger *log.Logger, conf *settings) []torrent {
-	// get session id?
-	// get a list of torrents from the endpoint in finished state
-	return nil
+func done(route string, session string) *[]torrent {
+	torrents := make([]torrent, 0)
+
+	c := http.Client{}
+	req, _ := http.NewRequest("POST", route, strings.NewReader(GetTorrents))
+	req.Header.Add("X-Transmission-Session-Id", session)
+	resp, _ := c.Do(req)
+
+	res := &response{}
+	decoder := json.NewDecoder(resp.Body)
+	decoder.Decode(res)
+
+	for _, t := range res.Args.Torrents {
+		if t.Finished {
+			torrents = append(torrents, t)
+		}
+	}
+
+	return &torrents
+}
+
+func session(route string) string {
+	resp, _ := http.Post(route, "json", strings.NewReader(GetSession))
+	return resp.Header.Get("X-Transmission-Session-Id")
+}
+
+func rpc(conf *settings) string {
+	return "http://127.0.0.1:" + strconv.Itoa(conf.Port) + conf.Uri + "rpc/"
 }
